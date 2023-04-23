b0abd1f22231ba46385c1344cc88ba247f22e9c9
Tunahan Kilic <tunahankilic942@gmail.com>
2023-04-23 15:51:57 +0300

    Infrastructure Layer was added.

---
+++ FormProject.Infrastructure/Repositories/FieldRepository.cs
@@ -0,0 +1,13 @@
+﻿using FormProject.Domain.Entities;
+using FormProject.Domain.Repositories;
+using FormProject.Infrastructure.DbContext;
+
+namespace FormProject.Infrastructure.Repositories
+{
+    public class FieldRepository : BaseRepository<Field>, IFieldRepository
+    {
+        public FieldRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
