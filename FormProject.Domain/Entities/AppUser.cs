d6ab5d1cddd00144584068f50df878c3d820df91
Tunahan Kilic <tunahankilic942@gmail.com>
2023-04-23 15:29:19 +0300

    Domain Layer was created.

---
+++ FormProject.Domain/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FormProject.Domain.Entities
+{
+    internal class AppUser : IdentityUser<int>
+    {
+        //Navigation Property
+        public List<Form> Forms { get; set; }
+    }
+}
